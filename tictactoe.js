6173e5b32e90b48ef26e2f2b0cfaf46cf229c795
Your Name <rishitvats1405@gmail.com>
2026-06-21 18:55:49 +0530

    tic tac toe part 1

---
+++ tictactoe.js
@@ -0,0 +1,29 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+
+let turn0 = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click" , () => {
+        if(turn0){
+        box.innerText = "O";
+        turn0 = false;
+    }
+        else{
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+    });
+});
